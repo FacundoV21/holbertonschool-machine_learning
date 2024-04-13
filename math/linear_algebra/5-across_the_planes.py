f486f3ba6f832df2e9f84481e15647328de68d76
FacundoV21 <6756@holbertonstudents.com>
2024-04-13 13:44:15 -0700

    fixed task 5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -26,10 +26,3 @@ def add_matrices2D(mat1, mat2):
             sum_matrix[i][j] = mat1[i][j] + mat2[i][j]
 
     return sum_matrix
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
-print(mat1)
-print(mat2)
-print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
