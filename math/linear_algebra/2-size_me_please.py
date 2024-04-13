12a23b1a974c7c9a15ee5c380d17ac94395b997b
FacundoV21 <6756@holbertonstudents.com>
2024-04-13 11:00:18 -0700

    added task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+"""
+    Task 2
+"""
 
 def matrix_shape(matrix):
+    """
+        func to return the size of a matrix
+    """
     if isinstance(matrix, list):
         return [len(matrix)] + matrix_shape(matrix[0])
     else:
