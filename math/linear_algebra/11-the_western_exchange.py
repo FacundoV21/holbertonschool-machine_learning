39e8a221a6685d3e885fc37a25295109e04b2efd
FacundoV21 <6756@holbertonstudents.com>
2024-04-14 12:06:36 -0700

    fixed errors

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
 """
     Task 11
 """
@@ -9,7 +8,5 @@ def np_transpose(matrix):
     """
         func to return transposed matrix
     """
-    new_matrix = np.array([])
-    new_matrix = np.transpose(matrix)
-
+    new_matrix = matrix.T
     return new_matrix
