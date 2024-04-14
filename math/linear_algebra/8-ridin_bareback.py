b64651f608ee25688acc8643a8d6a5fa84ffb9ce
FacundoV21 <6756@holbertonstudents.com>
2024-04-13 17:48:40 -0700

    added task 8 and fixed comment in task 7

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    Task 8
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+        func to return 2 multiplicated matrices
+    """
+    cols = len(mat1[0])
+    rows = len(mat2)
+
+    if cols != rows:
+        return None
+    
+    mat3 = [[0 for c in range(len(mat2[0]))] for r in range(len(mat1))]
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                mat3[i][j] += mat1[i][k] * mat2[k][j]
+    
+    return mat3
