6cd8f923e5d41d91bf8ec194610e9e572559436c
FacundoV21 <6756@holbertonstudents.com>
2024-04-14 12:02:11 -0700

    added tasks 9 to 11

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+    Task 10
+"""
+
+
+def np_shape(matrix):
+    """
+        func to return shape of an array
+    """
+    return matrix.shape
---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+    Task 11
+"""
+
+
+def np_transpose(matrix):
+    """
+        func to return transposed matrix
+    """
+    new_matrix = np.array([])
+    new_matrix = np.transpose(matrix)
+
+    return new_matrix
