626202c8c409efed8beb539d7f52b8f6d7dcbd5b
FacundoV21 <6756@holbertonstudents.com>
2024-04-17 16:22:42 -0700

    fixed task  0 and 1

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
 """
     task 0
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def line():
--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
 """
     task 1
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def scatter():
