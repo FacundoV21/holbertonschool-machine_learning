10ed64c0191936327db0df1cd49db3b91727f887
FacundoV21 <6756@holbertonstudents.com>
2024-04-17 16:17:48 -0700

    added task 1 and fixed 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""
+    task 0
+"""
+
 
 def line():
-
+    """
+        func to plot y as a line graph
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
