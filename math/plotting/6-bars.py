84c99d1d71b62fc58a28648a165d0bd6195d89ed
FacundoV21 <6756@holbertonstudents.com>
2024-04-18 15:34:52 -0700

    fixed task 6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -11,7 +11,7 @@ def bars():
         plot a stacked bar graph
     """
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
     colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
@@ -19,7 +19,8 @@ def bars():
     x = np.arange(3)
 
     for i in range(len(fruit)):
-        plt.bar(x, fruit[i], bottom=np.sum(fruit[:i], axis=0), color=colors[i], label=fruits[i], width=0.5)
+        plt.bar(x, fruit[i], bottom=np.sum(fruit[:i], axis=0),
+                 color=colors[i], label=fruits[i], width=0.5)
 
     plt.ylabel('Quantity of Fruit')
     plt.title('Number of Fruit per Person')
