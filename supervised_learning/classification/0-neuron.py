bf3da614e68b6d1b561cffc8cbb34ca6dc19f8e3
FacundoV21 <6756@holbertonstudents.com>
2024-05-07 13:51:50 -0700

    fixed tasks 0 to 4

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -20,6 +20,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.W = np.random.normal(scale=1.0, size=(nx,))
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -20,7 +20,7 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.__W = np.random.normal(scale=1.0, size=(nx,))
+        self.W = np.random.normal(size=(1, nx))
         self.__b = 0
         self.__A = 0
 
