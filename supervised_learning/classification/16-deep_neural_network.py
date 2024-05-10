b44ccaea245a9046045ab1697c5579fe798e6e84
FacundoV21 <6756@holbertonstudents.com>
2024-05-10 14:54:19 -0700

    fixed task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -28,8 +28,8 @@ class DeepNeuralNetwork:
             self.cache = {}
             self.weights = {}
             for i in range(self.L):
-                self.weights[f'W{i + 1}'] = np.random.normal(size=
-                    (layers[i], nx))*(np.sqrt(2 / nx))
+                self.weights[f'W{i + 1}'] = np.random.normal(size=(
+                    layers[i], nx))*(np.sqrt(2 / nx))
                 self.weights[f'b{i + 1}'] = np.zeros((layers[i], 1))
                 nx = layers[i]
 
