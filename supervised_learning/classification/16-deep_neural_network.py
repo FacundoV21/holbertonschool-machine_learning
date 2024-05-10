5cb0d69bbe56c06d0d29dbf636dbb0fed9df47bf
FacundoV21 <6756@holbertonstudents.com>
2024-05-10 14:52:16 -0700

    fixed task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -20,24 +20,18 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx <= 0:
             raise ValueError("nx must be a positive integer")
-
         if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
-        for num_nodes in layers:
-            if not isinstance(num_nodes, int) or num_nodes <= 0:
-                raise ValueError("layers must be a list of positive integers")
 
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
+        if all(map(lambda n: isinstance(n, int) and n > 0, layers)):
+            self.L = len(layers)
+            self.cache = {}
+            self.weights = {}
+            for i in range(self.L):
+                self.weights[f'W{i + 1}'] = np.random.normal(size=(layers[i], nx)
+                                                            )*(np.sqrt(2 / nx))
+                self.weights[f'b{i + 1}'] = np.zeros((layers[i], 1))
+                nx = layers[i]
 
-        for i, num_nodes in enumerate(layers, start=1):
-            if i == 1:
-                prev_nodes = nx
-            else:
-                prev_nodes = layers[i - 2]
-            fan_in = prev_nodes
-            self.weights[f"W{i}"] = np.random.randn(num_nodes, prev_nodes) * np.sqrt(2 / fan_in)
-            self.weights[f"b{i}"] = np.zeros((num_nodes, 1))
-
-        self.cache["A0"] = np.zeros((nx, 1))
+        else:
+            raise TypeError('layers must be a list of positive integers')
