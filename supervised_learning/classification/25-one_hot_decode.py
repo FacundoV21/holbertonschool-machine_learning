ba1895d4e704afcaaf391a794daedc9fc4f54f3a
FacundoV21 <6756@holbertonstudents.com>
2024-05-10 21:16:49 -0700

    fixed task 25

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -41,11 +41,15 @@ def one_hot_decode(one_hot):
     if type(one_hot) is not np.ndarray:
         return None
 
-    classes, m = one_hot.shape
+    try:
+        classes, m = one_hot.shape
 
-    if len(one_hot.shape) != 2 or one_hot.sum(axis=0).mean() != 1:
+        if len(one_hot.shape) != 2 or one_hot.sum(axis=0).mean() != 1:
+            return None
+
+        labels = np.argmax(one_hot, axis=1)
+
+        return labels
+
+    except Exception as ex:
         return None
-
-    labels = np.argmax(one_hot, axis=1)
-
-    return labels
