8506ba44477bac58b3ec731a5e9de2e68333de64
FacundoV21 <6756@holbertonstudents.com>
2024-05-10 21:55:55 -0700

    fixed task 26

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -140,7 +140,7 @@ class DeepNeuralNetwork:
         return Y_pred, cost
 
     def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True,
-               graph=True, step=100):
+            graph=True, step=100):
         """
             Trains the deep neural network.
         """
