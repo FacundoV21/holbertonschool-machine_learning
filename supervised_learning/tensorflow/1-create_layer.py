a9ebb54b2a7bf14bd7edefb4ef61455833183076
FacundoV21 <6756@holbertonstudents.com>
2024-05-11 14:16:55 -0700

    added task 1

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Tensorflow
+"""
+
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+  """
+    Creates a single layer in a TensorFlow neural network.
+  """
+
+  initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+
+  layer = tf.keras.layers.Dense(n, activation=activation, kernel_initializer=initializer, name='layer')(prev)
+
+  return layer
