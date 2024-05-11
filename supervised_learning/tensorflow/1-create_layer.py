ec652d70011d79f35364a5e00ad05bb80e123fe6
FacundoV21 <6756@holbertonstudents.com>
2024-05-11 14:19:23 -0700

    fixed task 1

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,14 +7,14 @@ import tensorflow.compat.v1 as tf
 
 
 def create_layer(prev, n, activation):
-  """
+    """
     Creates a single layer in a TensorFlow neural network.
-  """
+    """
 
-  initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-  layer = tf.keras.layers.Dense(n, activation=activation,
-                                 kernel_initializer=initializer,
+    layer = tf.keras.layers.Dense(n, activation=activation,
+                                  kernel_initializer=initializer,
                                   name='layer')(prev)
 
-  return layer
+    return layer
