5c350e03f4e869cbe2788888bc33ba732a236738
FacundoV21 <6756@holbertonstudents.com>
2024-05-19 14:30:39 -0700

    fixed task 4

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -2,18 +2,12 @@
 """
     task 4
 """
-import numpy as np
 
 
 def moving_average(data, beta):
     """
         Calculates the weighted moving average of a dataset with bias correction.
     """
-
-    if not isinstance(data, list) or beta <= 0 or beta > 1:
-        raise ValueError("Input must be a list of data points.\
-            Beta must be between 0 (exclusive) and 1 (inclusive).")
-
     moving_averages = []
     a=0
 
