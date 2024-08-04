17396b452cc75c167171a0ab012afb6c986d406d
FacundoV21 <6756@holbertonstudents.com>
2024-08-04 14:07:24 -0700

    fixed task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -6,7 +6,8 @@
 
 def np_elementwise(mat1, mat2):
     """
-        func performs element-wise addition, subtraction, multiplication, and division
+        func performs element-wise addition, subtraction,
+         multiplication, and division
     """
 
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
