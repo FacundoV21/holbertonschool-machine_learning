2179172c939951e24f6754464eda0a386f763e15
FacundoV21 <6756@holbertonstudents.com>
2024-08-04 13:55:31 -0700

    added tasks 9 to 14

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Task 11
+"""
+
+
+def np_transpose(matrix):
+    """
+        func to return transposed matrix
+    """
+    new_matrix = matrix.T
+    return new_matrix
