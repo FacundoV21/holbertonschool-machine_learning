8c7bf9c7b37bbcc37f62027fe39d82afee3ec2f2
FacundoV21 <6756@holbertonstudents.com>
2024-08-04 14:02:58 -0700

    fixed task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -3,17 +3,18 @@
     Task 12
 """
 
+import numpy as np
 
 def np_elementwise(mat1, mat2):
     """
         func performs element-wise addition, subtraction, multiplication, and division
     """
-    elementwise_sum = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    mat1 = np.array(mat1)
+    mat2 = np.array(mat2)
 
-    elementwise_dif = [[mat1[i][j] - mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
-
-    elementwise_mult = [[mat1[i][j] * mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
-
-    elementwise_div = [[mat1[i][j] / mat2[i][j] if mat2[i][j] != 0 else 0 for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    elementwise_sum = np.add(mat1, mat2)
+    elementwise_dif = np.subtract(mat1, mat2)
+    elementwise_mult = np.multiply(mat1, mat2)
+    elementwise_div = np.divide(mat1, mat2, out=np.zeros_like(mat1, dtype=float), where=mat2!=0)
 
     return elementwise_sum, elementwise_dif, elementwise_mult, elementwise_div
